1a9ece89164f7c4c4a5ff36023b4bd7858440fb5
JaimeLopezDiaz <jaime.lopdiaz@gmail.com>
2016-10-20 13:30:18 +0200

    CRUD

---
+++ personaSchema.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+var persona = new Schema({
+	nombre: String,
+	apellido:String,
+	DNI: String,
+	telefono:Number
+});
+
+module.exports = mongoose.model('nueva', persona);
